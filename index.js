1e849f2b01cc43f125543444c81600a019d7ccdf
ibraheem Hasan <rules656@gmail.com>
2019-12-08 17:57:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function testFunc() {
   return "hi"
 }
 function superbowlWin(record){
-let x=record.find(record => record.result === "W");
+let x=record.find(record=>record.result==="w");
 if(x === undefined)
 {
  return x
